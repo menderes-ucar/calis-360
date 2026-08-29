24a33bb55088c2e51c0ff460646afa1632dbf5da
Menderes Uçar <menderesucar10@gmail.com>
2026-08-29 19:12:58 +0300

    fix: configure Firebase Crashlytics Gradle plugin

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,15 +19,13 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
 
-    // Tek Android Gradle Plugin sürümü.
     id("com.android.application") version "8.7.3" apply false
 
-    // firebase-auth 24.2.x Kotlin 2.3 metadata ile derlendiği için
-    // Kotlin Gradle Plugin 2.3.x kullanıyoruz.
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
 
-    // Güncel Google Services Gradle plugin.
     id("com.google.gms.google-services") version "4.5.0" apply false
+
+    id("com.google.firebase.crashlytics") version "3.0.8" apply false
 }
 
 include(":app")
